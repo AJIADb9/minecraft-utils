f12875922f8dc73142763e237b022d1c5e20a1cc
AJIADb9 <ajiadb9@bgd.team>
2021-06-20 21:59:19 +0300

    tested test

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ SCREENS=$(screen -ls)
 
 if [[ $SCREENS == No* || $SCREENS != *MC* ]]; then
   echo Creatng screen
-  screen -dmS "MC" source /root/minecraft/server/start.sh
+  screen -dmS "MC" -c "${MC_SERVER_START_SH}"
   echo Starting Minecraft server
 
 else
